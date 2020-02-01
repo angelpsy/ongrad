647d97451f6d5642dc0a81b0385e8478434b3d9e
A.Alekseev <angelpsymail@gmail.com>
2020-02-01 14:09:31 +0500

    clearing code, add start code of project

---
+++ src/views/home.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="p-home">
+        home page
+    </div>
+</template>
+
+<script>
+export default {
+    name: "PageHome",
+};
+</script>
---
+++ vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    lintOnSave: false,
+    css: {
+        sourceMap: true,
+    },
+    devServer: {
+        host: "0.0.0.0",
+        port: 3000,
+        https: false,
+    },
+};
